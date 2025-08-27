06e7ed1146e4b6fcdc0ead9adf0f8d2671fc0ea1
b00tim <hulk996r@yahoo.com>
2025-08-28 00:25:23 +0200

    Update backend code: add classes & interface

---
+++ backend/src/main/java/movie/project/backend/MovieService.java
@@ -0,0 +1,20 @@
+package movie.project.backend;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+}
