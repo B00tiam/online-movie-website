3a30cd973c367a859fe816e0fcd9d47dbee3b242
b00tim <hulk996r@yahoo.com>
2025-08-29 17:51:55 +0200

    Update backend code: finish review controller

---
+++ backend/src/main/java/movie/project/backend/ReviewService.java
@@ -0,0 +1,33 @@
+package movie.project.backend;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        // insert the review
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        // update mongo database
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
