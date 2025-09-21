7cba7d600bdea37f6618727a81d553dd55951cc3
b00tim <hulk996r@yahoo.com>
2025-09-21 10:58:35 +0200

    Update frontend code: add trailer-play func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import api from './api/axiosConfig';
 import {useState, useEffect} from "react";
 import Layout from "./components/Layout";
 import {Routes, Route} from "react-router-dom";
+
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import Trailer from "./components/trailer/Trailer";
 
 function App() {
 
@@ -40,6 +42,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
         </Route>
       </Routes>
 
